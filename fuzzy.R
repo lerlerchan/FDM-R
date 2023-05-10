c16f343dec0d5c41ed5f9f0baacdcb02e6e31736
lerlerchan <lkchan@sc.edu.my>
2023-05-10 13:16:30 +0800

    update
    update

--- fuzzy.R
+++ fuzzy.R
@@ -1,13 +1,21 @@
-x <- c(0.7, 0.8)
-y <- c(0.2, 0.3)
-## Use default family ("Zadeh")
-.N.(x)
-.T.(x, y)
-.S.(x, y)
-.I.(x, y)
-## Switch family and try again
-fuzzy_logic("Fodor")
-.N.(x)
-.T.(x, y)
-.S.(x, y)
-.I.(x, y)
+# Import the sets library
+library(sets)
+
+# Create a fuzzy set for temperature
+cold <- fuzzy_set(name = "cold", membership = function(x) {
+  ifelse(x < 20, 1, 0)
+})
+
+hot <- fuzzy_set(name = "hot", membership = function(x) {
+  ifelse(x > 30, 1, 0)
+})
+
+# Create a fuzzy variable for temperature
+temperature <- fuzzy_variable(name = "temperature", sets = c(cold, hot))
+
+# Set the temperature to 40 degrees Celsius
+temperature$set(40)
+
+# Plot the fuzzy sets and the temperature
+plot(temperature)
+
