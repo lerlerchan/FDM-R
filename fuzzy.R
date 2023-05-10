ba9dfee41dd48063c94e6e3eb365aa5320d83cb0
lerlerchan <lkchan@sc.edu.my>
2023-05-10 12:26:15 +0800

    update
    update

--- fuzzy.R
+++ fuzzy.R
@@ -1,11 +1,13 @@
-library(sets)
-
-# Define a universal set for age
-age_univ <- FuzzySet(seq(0, 100, by = 1), rep(1, 101), name = "age")
-
-# Define a fuzzy set for young age
-age_young <- FuzzySet(c(0, 0, 25, 35), c(1, 0, 0, 0), name = "young")
-age_young <- FuzzySet(age_univ, name="young")
-
-# Plot the fuzzy set
-plot(age_univ, age_young, main = "Young Age")
+x <- c(0.7, 0.8)
+y <- c(0.2, 0.3)
+## Use default family ("Zadeh")
+.N.(x)
+.T.(x, y)
+.S.(x, y)
+.I.(x, y)
+## Switch family and try again
+fuzzy_logic("Fodor")
+.N.(x)
+.T.(x, y)
+.S.(x, y)
+.I.(x, y)
